175bd372a67ff49e2d14f425b5e5420165b3dd54
wxnacy <371032668@qq.com>
2023-02-16 19:27:51 +0800

    docs(formatter): 增加 black 对比文档

--- lua/formatter/config.lua
+++ lua/formatter/config.lua
@@ -2,7 +2,9 @@ local M = {}
 
 -- 服务列表
 M.servers = {
+    -- "black",         -- python
     "autopep8",         -- python
+    -- black vs autopep8 https://juejin.cn/s/python%20formatter%20autopep8%20vs%20black%20vs%20yapf
     "markdownlint",     -- markdown
     "eslint_d",         -- js
     "prettierd",        -- css
