7e6533336efee7268e121d7698e2e547a4945dbc
wxnacy <wxnacy@gmail.com>
2023-04-09 08:27:35 +0800

    refactor(packer): startup 参数改回到自支持的方式, 删除 load_config 方法

--- lua/plugins/packer/loader.lua
+++ lua/plugins/packer/loader.lua
@@ -1,28 +1,6 @@
 local M = {}
 local utils = require("utils")
 
--- 加载 config 方法
-M.load_config = function (plugin)
-    if type(plugin) ~= 'table' then
-        return
-    end
-
-    local first_sepc = plugin[1]
-    if type(first_sepc) == 'table' then
-        for _, sub_plugin in ipairs(plugin) do
-            M.load_config(sub_plugin)
-        end
-    else
-        local name, _ = require("packer.util").get_plugin_short_name(plugin)
-        -- print(name)
-        -- print(_G.packer_plugins[name])
-        -- print(_G.packer_plugins[name].loaded)
-        if plugin.config and require("utils").is_install_plugin(name) then
-            pcall(plugin.config)
-        end
-    end
-end
-
 M.install_packer = function ()
     -- 下载 packer
     local is_new_install
@@ -58,14 +36,8 @@ M.setup = function (configs)
     end
     vim.cmd [[packadd packer.nvim]]
 
-    local startup = function (use)
-        for _, plugin in ipairs(configs) do
-            use(plugin)
-            M.load_config(plugin)
-        end
-    end
     packer.reset()
-    packer.startup(startup)
+    packer.startup({configs})
 
     -- 新装插件时安装全部
     if is_new_install then
