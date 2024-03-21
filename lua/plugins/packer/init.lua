c3cf50ac34db255cfa0bf028ddbe04c7876a73ec
wxnacy <371032668@qq.com>
2024-03-21 17:03:47 +0800

    fix(telescope): 修改版本为 0.1.6 解决 User TelescopeFilePre 报错

--- lua/plugins/packer/init.lua
+++ lua/plugins/packer/init.lua
@@ -267,7 +267,7 @@ local tool_plugins = {
         -- 搜索工具
         {
             'nvim-telescope/telescope.nvim',
-            tag = '0.1.0',
+            tag = '0.1.6',
             requires = { { 'nvim-lua/plenary.nvim' } },
             config = function()
                 require("plugins.config.telescope")
