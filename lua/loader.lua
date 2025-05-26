b6cc6f10f727d0d812b82597659013c6cdc83eb8
wxnacy <wxnacy@gmail.com>
2025-05-26 22:23:56 +0800

    feat(plugins): 调整插件启动方式

--- lua/loader.lua
+++ lua/loader.lua
@@ -10,21 +10,6 @@ local function load_config()
     end
 end
 
-local function load_plugin()
-    -- 加载插件
-    -- require('plugins').setup()
-    require('plugins.packer')
-    require('lsp')
-    require('autocmds')
-
-    -- 加载本地插件
-    local local_plugin_home = config.CONFIG_HOME
-    if utils.is_dir_exists(local_plugin_home) then
-        package.path = local_plugin_home .. "/?.lua;" .. package.path
-        package.path = local_plugin_home .. "/?/init.lua;" .. package.path
-    end
-end
-
 -- 加载项目配置
 -- 在最后加载
 local function load_project_config()
@@ -40,7 +25,7 @@ M.setup = function ()
     load_config()
     require("autocmds")
     -- load_plugin()
-    require("config.lazy").setup()
+    require("plugins")
     load_project_config()
     -- 放在最后，避免被插件的配置覆盖
     require('basic')
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,5 +1,41 @@
-return {
-    require("plugins.colorscheme"),
-    require("plugins.editor"),
-    require("plugins.coder"),
-}
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+local M = {}
+
+M.install = function()
+	-- Bootstrap lazy.nvim
+	if not (vim.uv or vim.loop).fs_stat(lazypath) then
+		local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+		local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+		if vim.v.shell_error ~= 0 then
+			vim.api.nvim_echo({
+				{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+				{ out, "WarningMsg" },
+				{ "\nPress any key to exit..." },
+			}, true, {})
+			vim.fn.getchar()
+			os.exit(1)
+		end
+	end
+end
+
+M.setup = function()
+	M.install()
+	vim.opt.rtp:prepend(lazypath)
+	-- Setup lazy.nvim
+	require("lazy").setup({
+		spec = {
+			-- import your plugins
+			{ import = "plugins.colorscheme" },
+			{ import = "plugins.editor" },
+			{ import = "plugins.coder" },
+		},
+		-- Configure any other settings here. See the documentation for more details.
+		-- colorscheme that will be used when installing plugins.
+		install = { colorscheme = { "catppuccin" } },
+		-- automatically check for plugin updates
+		checker = { enabled = true },
+	})
+	vim.keymap.set("n", "<leader>l", "<Cmd>Lazy<CR>")
+end
+--return M
+M.setup()
