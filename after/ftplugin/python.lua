a1bca06bc11f8e2f8ed5edf4e6cc1dfc8972637a
wxnacy <wxnacy@gmail.com>
2025-05-29 12:03:50 +0800

    feat(plugins/nerdcommenter): 除了 python 都设置注释后边加空格

---
+++ after/ftplugin/python.lua
@@ -0,0 +1,2 @@
+-- python 默认有一个空格，不在设置
+vim.g.NERDSpaceDelims = 0 -- 注释后面保留一个空格，新版本会默认有一个空格
--- lua/plugins/coder.lua
+++ lua/plugins/coder.lua
@@ -1,68 +1,65 @@
 return {
-    -- 代码补全
-    require("plugins.lazy.blink"),
-    -- lsp
-    require("plugins.lazy.lsp"),
-    -- 代码格式化
-    require("plugins.lazy.formatter"),
-    -- 代码语法高亮
-    require("plugins.lazy.treesitter"),
-    -- 搜索增强
-    require("plugins.lazy.telescope"),
-    require("plugins.lazy.trouble"),
-    -- 代码折叠
-    require("plugins.lazy.fold"),
-    -- for golang
-    { 'fatih/vim-go' },
+	-- 代码补全
+	require("plugins.lazy.blink"),
+	-- lsp
+	require("plugins.lazy.lsp"),
+	-- 代码格式化
+	require("plugins.lazy.formatter"),
+	-- 代码语法高亮
+	require("plugins.lazy.treesitter"),
+	-- 搜索增强
+	require("plugins.lazy.telescope"),
+	require("plugins.lazy.trouble"),
+	-- 代码折叠
+	require("plugins.lazy.fold"),
+	-- for golang
+	{ "fatih/vim-go" },
 
-    -- https://github.com/folke/todo-comments.nvim
-    {
-        "folke/todo-comments.nvim",
-        dependencies = { "nvim-lua/plenary.nvim" },
-        opts = {
-            -- your configuration comes here
-            -- or leave it empty to use the default settings
-            -- refer to the configuration section below
-        }
-    },
+	-- https://github.com/folke/todo-comments.nvim
+	{
+		"folke/todo-comments.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		opts = {
+			-- your configuration comes here
+			-- or leave it empty to use the default settings
+			-- refer to the configuration section below
+		},
+	},
 
-    -- 快速去掉行尾空格
-    -- https://github.com/ntpeters/vim-better-whitespace
-    {
-        'ntpeters/vim-better-whitespace',
-        init = function()
-            vim.keymap.set('n', '<leader><space>', '<Cmd>StripWhitespace<CR>')
-            -- 展示 snacks_dashboard 时不高亮空格
-            -- https://github.com/ntpeters/vim-better-whitespace/issues/172#issuecomment-2778894757
-            vim.api.nvim_create_autocmd("User", {
-                pattern = "SnacksDashboardOpened",
-                callback = function() vim.cmd [[DisableWhitespace]] end,
-            })
-        end
-    },
+	-- 快速去掉行尾空格
+	-- https://github.com/ntpeters/vim-better-whitespace
+	{
+		"ntpeters/vim-better-whitespace",
+		init = function()
+			vim.keymap.set("n", "<leader><space>", "<Cmd>StripWhitespace<CR>")
+			-- 展示 snacks_dashboard 时不高亮空格
+			-- https://github.com/ntpeters/vim-better-whitespace/issues/172#issuecomment-2778894757
+			vim.api.nvim_create_autocmd("User", {
+				pattern = "SnacksDashboardOpened",
+				callback = function()
+					vim.cmd([[DisableWhitespace]])
+				end,
+			})
+		end,
+	},
 
+	-- 快速注释
+	-- https://github.com/scrooloose/nerdcommenter
+	{
+		"scrooloose/nerdcommenter",
+		config = function()
+			-- 新版本会默认有一个空格
+			vim.g.NERDSpaceDelims = 1 -- 注释后面保留一个空格，新版本会默认有一个空格
+		end,
+	},
 
-    -- 快速注释
-    -- https://github.com/scrooloose/nerdcommenter
-    {
-        'scrooloose/nerdcommenter',
-        config = function()
-            -- 新版本会默认有一个空格
-            -- vim.g.NERDSpaceDelims = 1 -- 注释后面保留一个空格，新版本会默认有一个空格
-        end
-    },
-
-    -- 签名增强
-    -- https://github.com/ray-x/lsp_signature.nvim
-    {
-        "ray-x/lsp_signature.nvim",
-        event = "InsertEnter",
-        opts = {
-            -- cfg options
-        },
-    },
-
-
-
-
+	-- 签名增强
+	-- https://github.com/ray-x/lsp_signature.nvim
+	{
+		"ray-x/lsp_signature.nvim",
+		event = "InsertEnter",
+		opts = {
+			-- cfg options
+		},
+	},
 }
