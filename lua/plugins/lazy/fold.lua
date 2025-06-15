ad63daa62b4dabd681126901286920efd0c1854e
wxnacy <wxnacy@gmail.com>
2025-06-15 12:58:32 +0800

    feat(plugins/fold): 自定义折叠代码后缀

--- lua/plugins/lazy/fold.lua
+++ lua/plugins/lazy/fold.lua
@@ -1,5 +1,35 @@
 -- 代码折叠
 -- https://github.com/kevinhwang91/nvim-ufo
+
+-- 自定义折叠后缀
+local handler = function(virtText, lnum, endLnum, width, truncate)
+    local newVirtText = {}
+    local suffix = (' 󰁂 %d '):format(endLnum - lnum)
+    local sufWidth = vim.fn.strdisplaywidth(suffix)
+    local targetWidth = width - sufWidth
+    local curWidth = 0
+    for _, chunk in ipairs(virtText) do
+        local chunkText = chunk[1]
+        local chunkWidth = vim.fn.strdisplaywidth(chunkText)
+        if targetWidth > curWidth + chunkWidth then
+            table.insert(newVirtText, chunk)
+        else
+            chunkText = truncate(chunkText, targetWidth - curWidth)
+            local hlGroup = chunk[2]
+            table.insert(newVirtText, {chunkText, hlGroup})
+            chunkWidth = vim.fn.strdisplaywidth(chunkText)
+            -- str width returned from truncate() may less than 2nd argument, need padding
+            if curWidth + chunkWidth < targetWidth then
+                suffix = suffix .. (' '):rep(targetWidth - curWidth - chunkWidth)
+            end
+            break
+        end
+        curWidth = curWidth + chunkWidth
+    end
+    table.insert(newVirtText, {suffix, 'MoreMsg'})
+    return newVirtText
+end
+
 return {
     "kevinhwang91/nvim-ufo",
     dependencies = "kevinhwang91/promise-async",
@@ -18,6 +48,7 @@ return {
             provider_selector = function(bufnr, filetype, buftype)
                 return { "treesitter", "indent" }
             end,
+            fold_virt_text_handler = handler,
         })
         -- 键盘映射,这里的按键会打开或折叠全部的可折叠位置
         vim.keymap.set("n", "zR", require("ufo").openAllFolds)
