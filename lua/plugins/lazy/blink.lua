258e958af48b231c374f6ebf58b8c7f79adf5ec4
wxnacy <wxnacy@gmail.com>
2025-11-10 12:53:28 +0800

    feat(blink): 取消补全列表

--- lua/plugins/lazy/blink.lua
+++ lua/plugins/lazy/blink.lua
@@ -38,6 +38,8 @@ return {
 			["<C-p>"] = { "select_prev", "snippet_backward", "fallback" },
 			["<C-n>"] = { "select_next", "snippet_forward", "fallback" }, -- 同时存在补全列表和snippet时，补全列表选择优先级更高
 
+			["<C-x>"] = { "hide", "fallback" }, -- 取消补全列表
+
 			["<C-b>"] = { "scroll_documentation_up", "fallback" },
 			["<C-f>"] = { "scroll_documentation_down", "fallback" },
 
